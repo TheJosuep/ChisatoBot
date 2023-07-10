cf99d1f19709cff32cc24a65618d103a5ed0b787
DJosuep <josuep.garciar@gmail.com>
2023-07-10 00:36:10 -0600

    Refactor function and commands
    - Add command to get guild bans and show them.
    - Add unban command (still not working).
    - Refactor ban_permissions() to higher_hierarchy() in administration file.
    - Add say command.

--- commands/administration.py
+++ commands/administration.py
@@ -1,5 +1,6 @@
 import discord, typing
 from discord.ext import commands
+from datetime import datetime
 
 # CHECKS
 
@@ -7,7 +8,7 @@ class MissingPermissions(commands.CheckFailure):
     pass
 
 # Check if the user trying to ban has a higher role than the other member
-def ban_permissions(ctx, member):
+def higher_hierarchy(ctx, member):
     return ctx.author.top_role > member.top_role
 
 # Check if
@@ -38,21 +39,59 @@ class Administration(commands.Cog):
         givenReason = '.' if reason is None else ' por ' + reason + '.'
 
         if len(bannedMembers) > 1:
-            await ctx.reply(f"{membersMention} fueron baneados{givenReason}")
+            await ctx.send(f"{membersMention} fueron baneados{givenReason}")
         elif len(bannedMembers) == 1:
-            await ctx.reply(f"{membersMention} fue baneado{givenReason}")
+            await ctx.send(f"{membersMention} fue baneado{givenReason}")
         else:
-            await ctx.send("¡Debes mencionar al menos a un miembro!")
+            await ctx.reply("¡Debes mencionar al menos a un miembro!")
     
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send(error)
     
-    @commands.has_permissions(administrator = True)
-    @commands.command(help = "Desbanea a un usuario")
-    async def unban(ctx, *, member):
-        bannedUsers = await ctx.guild.bans()
+    @commands.command(help = "Muestra la lista de usuarios baneados.")
+    @has_ban_permissions()
+    async def bans(self, ctx):
+        # Defers the interaction due to a long task
+        await ctx.defer()
+        # Gets the ban entries that contains an user and a reason (optional)
+        bans = [entry async for entry in ctx.guild.bans()]
+
+        embed = discord.Embed(title = f"Usuarios baneados de {ctx.guild.name}.",
+                              timestamp = datetime.now(),
+                              color = discord.Colour.pink())
+        if len(bans) > 0:    
+            for entry in bans:
+                if len(embed.fields) > 25:
+                    break
+                if len(embed) > 5900:
+                    embed.field(name = "Demasiados elementos en la lista.")
+                else:
+                    embed.add_field(name = entry.user.name, value = f"ID del usuario: {entry.user.id} \nRazón: {'Sin especificar.' if entry.reason is None else entry.reason} \nBot: {'No' if entry.user.bot is False else 'Sí'}",
+                                    inline = False)
+            
+            await ctx.reply(embed = embed)
+        else:
+            await ctx.reply("No hay usuarios baneados en el servidor.")
+                
+
+    @commands.command(help = "Quita el ban a un usuario: c.ban <usuario>")
+    @has_ban_permissions()
+    async def unban(self, ctx, user = None, *, reason: str = None):
+        bannedUsers = [entry async for entry in ctx.guild.bans()]
+        givenReason = '.' if reason == None else ' por ' + reason + '.'
+
+        if user is None:
+            ctx.reply("Debe ingresar un nombre o el id de un usuario.")
+        else:
+            if isinstance(user, int):
+                for bannedUser in bannedUsers:
+                    if bannedUser.user.id == user:
+                        await bannedUser.unban(bannedUser, reason)
+                        await ctx.send(f"El usuario {bannedUser.user.name} fue desbaneado{givenReason}")
+                
+                ctx.reply("El usuario no ha sido encontrado en la lista de baneados.")
 
 async def setup(bot):
     await bot.add_cog(Administration(bot))
